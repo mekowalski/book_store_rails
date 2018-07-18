8860195475089704aa99b502c456edffb1c0fa13
Malind Kowalski <malind.nikulski@gmail.com>
2018-07-18 16:49:14 -0600

    trying to create ruby def to match criteria in lines 1-3

---
+++ add_stuff_to_db.rb
@@ -0,0 +1,9 @@
+#need to find the books
+#create a category of ScienceFiction
+#relate the category of ScienceFiction tot he existing books that have an author named Any Weir
+
+def add_category_to_authors_books(category_title, author_name)
+  author = Author.find_by(name: 'Andy Weir')
+  category = Category.find_or_create_by(title: category_title)
+  author.books.each do |book|
+end
