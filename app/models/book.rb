5bc637722a3719e84ff36bb88e752d1fca462964
Malind Kowalski <malind.nikulski@gmail.com>
2018-08-06 13:52:01 -0600

    add dependent destroy to models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :author
-  has_many :book_categories
+  has_many :book_categories, dependent: :destroy
   has_many :categories, through: :book_categories
 end
