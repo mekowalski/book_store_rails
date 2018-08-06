90ba2ed4bea622d57bec77374638c2652961cfb9
Malind Kowalski <malind.nikulski@gmail.com>
2018-08-06 10:33:36 -0600

    word correction

--- add_stuff_to_db.rb
+++ add_stuff_to_db.rb
@@ -1,6 +1,6 @@
 #need to find the books
 #create a category of ScienceFiction
-#relate the category of ScienceFiction tot he existing books that have an author named Any Weir
+#relate the category of ScienceFiction to the existing books that have an author named Any Weir
 
 def add_category_to_authors_books(category_title, author_name)
   author = Author.find_by(name: 'Andy Weir')
