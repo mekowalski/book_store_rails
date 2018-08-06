9f8ec310d071f90dfb983a706bf40330d7e14fa8
Malind Kowalski <malind.nikulski@gmail.com>
2018-08-06 10:49:08 -0600

    correct find_by method and figuring out issues with method

--- add_stuff_to_db.rb
+++ add_stuff_to_db.rb
@@ -3,7 +3,13 @@
 #relate the category of ScienceFiction to the existing books that have an author named Any Weir
 
 def add_category_to_authors_books(category_title, author_name)
-  author = Author.find_by(name: 'Andy Weir')
+  author = Author.find_by(name: author_name)
   category = Category.find_or_create_by(title: category_title)
   author.books.each do |book|
+    book.categories << category
+  end
 end
+
+#issues with this
+#1. making tons of commits and transactions
+#2. creating the same item multiple times
